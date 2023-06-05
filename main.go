445bff14685ec25dcbae2afcf9a989f6ffb981aa
Brayden Ooi <brayden.ooi@hotmail.com>
2023-06-05 16:30:56 +0800

    add create feed_follows logic

--- main.go
+++ main.go
@@ -59,6 +59,9 @@ func main() {
 
 	v1Router.Post("/feeds", apiCfg.AuthMiddleware(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Post("/feed_follows", apiCfg.AuthMiddleware(apiCfg.handlerCreateFeedFollow))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
