3385a83d327d16c039f983bf26a767829b62cce7
Brayden Ooi <brayden.ooi@hotmail.com>
2023-06-05 15:37:04 +0800

    refactor auth logic to middleware

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 	v1Router.Get("/health", handlerReadiness)
 	v1Router.Get("/err", handlerError)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
-	v1Router.Get("/users", apiCfg.handlerGetUser)
+	v1Router.Get("/users", apiCfg.AuthMiddleware(apiCfg.handlerGetUser))
 
 	router.Mount("/v1", v1Router)
 
